a30ce60a26a28a6336be33875f60ae4d5a20b958
bingulo <gcarlos@disroot.org>
2022-07-12 21:45:30 -0300

    Refeita lógica de movimentação e alterado tamanho do canvas

--- main.js
+++ main.js
@@ -2,115 +2,143 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
+// Canvas principal
 const canvas = document.getElementById('main_canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = canvas.height =400; //tamanho da tela 
+// Tamanho da tela do jogo
+canvas.width = canvas.height = 600;
+const FRAMERATE = 10;
 
-const FRAMERATE = 10; //framerate
-const SIZE = 20;  //grossura da cobra
-const BORDA = canvas.width / SIZE; //tamanho da borda do mapa 
+// Tamanho das unidade
+const SIZE = 20;
+
+// Borda do mapa 
+const BORDA = canvas.width / SIZE;
 
 let pos, vel, food, snake;
 
- var dir = 4000002; //variavel que controla a direção da cobra 
+// Variável que controla a direção da cobra 
+var dir;
 
-function init(){ //lista da posição inicial da cobra 
-  pos = {x: 10, y: 10};
-  vel = {x: 0, y: 0};
+function init(){ 
+    // Posição inicial da cobra
+    pos = {x: 10, y: 10};
+    vel = {x: 0, y: 0};
 
-   dir = 40000002;
+    dir = 2;
 
-  snake = [ //lista das celulas da cobra 
-    {x: 8, y: 10},
-    {x: 9, y: 10},
-    {x: 10, y: 10},
-  ]
+    // Celulas da cobra 
+    snake = [ 
+        {x: 8, y: 10},
+        {x: 9, y: 10},
+        {x: 10, y: 10}
+    ]
 
-  randomFood();
+    randomFood();
 }
 
 init();
 
 function randomFood(){
-  food = {
-    x: Math.floor(Math.random() * BORDA), //gera posição da comida nova, dentro das bordas
-    y: Math.floor(Math.random() * BORDA),
-  }
-
-  for (let cell of snake) {
-    if(cell.x === food.x && food.y === cell.y) { //checa se a comida gerada não esta na mesma posição da cobra  
-      return randomFood();
+    // Gera posição da comida nova dentro das bordas
+    food = {
+        x: Math.floor(Math.random() * BORDA), 
+        y: Math.floor(Math.random() * BORDA),
     }
-  }
+    // Checa se a comida gerada não esta na mesma posição da cobra  
+    for (let cell of snake) {
+        if(cell.x === food.x && food.y === cell.y) { 
+            return randomFood();
+        }
+    }
 }
 
 document.addEventListener('keydown', keydown);
-
 function keydown(e){
-  switch(e.keyCode) { //direção da cobra, os botões de controle são direita e esquerda 
-    case 37: { 
-      dir = dir -1;
-      break;
+    switch(e.keyCode) {
+        // Tecla <-
+        case 37: { 
+            dir--;
+            break;
+        }
+        // Tecla ->
+        case 39: {
+            dir++;
+            break;
+        }
     }
-    case 39: {
-      dir = dir +1;
-      break;
+
+    dir += 4;
+    dir %= 4;
+    switch(dir) {
+        case 0: {
+            // Esquerda
+            vel = {x: -1, y: 0};
+            break;
+        }
+        case 1: {
+            // Baixo
+            vel = {x: 0, y: -1};
+            break;
+        }
+        case 2: {
+            // Direita
+            vel = {x: 1, y: 0};
+            break;
+        }
+        case 3: {
+            // Cima
+            vel = {x: 0, y: 1}
+            break;
+        }
     }
-  }
-  if(dir%4 == 0){
-    return vel = {x: -1, y: 0}
-  }
-  if(dir%4 == 1){
-    return vel = {x: 0, y: -1}
-  }
-  if(dir%4 == 2){
-    return vel = {x: 1, y: 0}
-  }
-  if(dir%4 == 3){
-    return vel = {x: 0, y: 1}
-  }
 }
 
-
-
 setInterval(() => {
-  requestAnimationFrame(gameLoop); //atualiza o jogo a um framerate 
+    requestAnimationFrame(gameLoop); 
 }, 1000 /FRAMERATE);
 
 function gameLoop(){
-  ctx.fillStyle = BG_COLOUR;
-  ctx.fillRect(0, 0, canvas.width, canvas.height); //preenche a arena do jogo
+    // Preenche o fundo da arena
+    ctx.fillStyle = BG_COLOUR;
+    ctx.fillRect(0, 0, canvas.width, canvas.height); 
 
-  ctx.fillStyle = SNAKE_COLOUR;
-  for (let cell of snake) {
-    ctx.fillRect(cell.x*SIZE, cell.y*SIZE, SIZE,SIZE); //preenche a cobra 
-  }
-
-  ctx.fillStyle = FOOD_COLOUR;
-  ctx.fillRect(food.x*SIZE,food.y*SIZE,SIZE,SIZE); //preenche a comida 
-
-  pos.x += vel.x; //movimento, faz proxima posição 
-  pos.y += vel.y;
-
-  if (pos.x < 0 || pos.x > BORDA-1 || pos.y < 0 || pos.y > BORDA-1) { //se toca nas bordas, reinicia o jogo
-    init();
-  }
-
-  if (food.x === pos.x && food.y === pos.y) {
-    snake.push({...pos});
-    pos.x += vel.x;
-    pos.y += vel.y;
-    randomFood();
-  }
-
-  if (vel.x || vel.y) {
+    // Preenche cada célula da cobra
+    ctx.fillStyle = SNAKE_COLOUR;
     for (let cell of snake) {
-      if (cell.x === pos.x && cell.y === pos.y) { //se a cobra toca nela mesma, reinicia o jogo
-        return init();
-      }
+        ctx.fillRect(cell.x*SIZE, cell.y*SIZE, SIZE,SIZE); 
     }
-    snake.push({...pos}); //coloca um elemento no final da lista
-    snake.shift(); //remove o elemento do começo da lista 
-  }
+    
+    // Preenche a comida 
+    ctx.fillStyle = FOOD_COLOUR;
+    ctx.fillRect(food.x*SIZE,food.y*SIZE,SIZE,SIZE);
+
+    pos.x += vel.x; 
+    pos.y += vel.y;
+    
+    // Se tocar nas bordas, reinicia o jogo
+    if (pos.x < 0 || pos.x === BORDA || pos.y < 0 || pos.y === BORDA) { 
+        init();
+    }
+
+    if (food.x === pos.x && food.y === pos.y) {
+        snake.push({...pos});
+        pos.x += vel.x;
+        pos.y += vel.y;
+        randomFood();
+    }
+
+    if (vel.x || vel.y) {
+        for (let cell of snake) {
+            // Se a cobra toca nela mesma, reinicia o jogo
+            if (cell.x === pos.x && cell.y === pos.y) { 
+                return init();
+            }
+        }
+        // Coloca o elemento "pos" no final da lista
+        snake.push({...pos}); 
+        // Remove o elemento do começo da lista 
+        snake.shift(); 
+    }
 }
